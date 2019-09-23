b892debfd7c2c5919a8d51e234458d7672af09ab
asafrahimi <55393002+asafrahimi@users.noreply.github.com>
2019-09-23 17:00:27 +0300

    Delete weather.py

--- weather.py
+++
@@ -1,7 +0,0 @@
-#import json
-import requests
-City = input('Hello\nPlease enter city')
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+City+'&APPID=4c151bd4d9683df3c1565451375278e3')
-response = r.json()
-Temp = ((response['main'])['temp']) - 273
-print('The tempreture in',City, 'now is', Temp , 'Deg Celcious')
